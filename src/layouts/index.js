623a1e49c4ce5749748b0ce40042da9e13681c9c
mexar <f.aubeut@gmail.com>
2018-05-04 17:38:23 +0200

    change repo

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styled from "styled-components"
 
-import './index.css'
 import 'semantic-ui-css/semantic.min.css';
 
 const Layout = ({ children, data }) => (
@@ -14,5 +14,19 @@ const Layout = ({ children, data }) => (
 Layout.propTypes = {
   children: PropTypes.func,
 }
+
+const Wrapper = styled.div`
+body, html {
+  overflow-x: hidden !important;
+}
+
+*:not(i){
+  font-family: 'Open Sans', sans-serif !important;
+}
+
+.header, h1,h2,h3,h4,h5,h6 {
+  font-family: 'Muli', sans-serif !important;
+}
+`
 
 export default Layout
